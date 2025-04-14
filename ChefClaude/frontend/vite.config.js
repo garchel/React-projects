66e43dc89c50518816a3f8405683ecdd4dc6cb6e
garchel <paulovictor.co97@gmail.com>
2025-04-14 15:57:21 -0300

    added gh-pages

--- ChefClaude/frontend/vite.config.js
+++ ChefClaude/frontend/vite.config.js
@@ -1,8 +1,23 @@
-import {defineConfig} from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-	plugins: [
-		react()
-	]
+  base: '/React-projects/ChefClaude/',
+  plugins: [react()],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      external: ['axios', 'react-markdown'],
+      output: {
+        globals: {
+          axios: 'axios',
+          'react-markdown': 'ReactMarkdown'
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['axios', 'react-markdown']
+  }
 })
--- ChefClaude/vite.config.js
+++ ChefClaude/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/React-projects/ChefClaude/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  root: './'
 })
